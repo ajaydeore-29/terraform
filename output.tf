4a6a201098afd99fc4af43a72924ed2aaa17f297
ajaydeore-29 <ajaydeore8790@gmail.com>
2026-09-02 17:34:57 +0530

    day 1

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
-    value = aws_instance.ec2.public_ip
-    
+    value = aws_instance.my_ec2.public_ip
+
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    proffile = "dev"
+    profile = "dev"
 
 }

6b212852e91860b72ac3c6187958975691c934b8
ajaydeore-29 <ajaydeore8790@gmail.com>
2026-09-02 17:10:48 +0530

    day 1

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
-resource "aws_instance" "my_ec2" {
-    ami = 01a00762f46d584a1
-    instance_type = t3.micro
-    key_name = keypair.pem
+provider "aws" {
+  region = "ap-south-1"
 }
 
-tags  = {
-    name = "ec2"
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+  key_name      = "keypair"
+
+  tags = {
+    Name = "ec2"
+  }
 }

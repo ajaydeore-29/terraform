a108c314c3fcee5d0367a9224ab4ab3d80cc74f9
ajaydeore-29 <ajaydeore8790@gmail.com>
2026-09-08 01:13:50 +0530

    update

--- day-terraform-var-file/main.tf
+++ day-terraform-var-file/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
     source = "./module/vpc"
-    vpc_id = var.vpc_cidr {}
-    public_subnet = var.public_subnet_cidr {}
-    private_subnet = var.private_subnet_cidr {}
+    vpc_id = var.vpc_id {}
+    public_subnet = var.public_subnet_id {}
+    private_subnet = var.private_subnet_id {}
     public_az = var.public_az {}
     private_az = var.private_az
     ssh_port = var.ssh_port {}

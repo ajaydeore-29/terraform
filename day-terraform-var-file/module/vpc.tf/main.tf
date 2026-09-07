fae6337c5c947cabef1ef09bca177c2a4711b947
ajaydeore-29 <ajaydeore8790@gmail.com>
2026-09-07 23:48:20 +0530

    day-5

---
+++ day-terraform-var-file/module/vpc.tf/main.tf
@@ -0,0 +1,112 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags {
+        Name = "my_vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "10.0.0.0/20
+    availability_zone = "ap_south_1a"
+    map_public_on_launch = true
+    tags {
+        Name = "public_subnet"
+    }
+}
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "10.0.0.0/20
+    availability_zone = "ap_south_1b"
+    map_public_on_launch = false
+    tags {
+        Name = "private_subnet"
+    }
+}
+
+resource "aws_internet_gateway" {
+    vpc_id = aws_vpc.my_vpc.id
+    tags {
+        Name = "igw"
+    }
+}
+
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+    tags {
+        Name = "nat_eip
+    }
+
+}
+
+resource "aws_nat_gateway" "nat"{
+     subnet_id = aws_subnet.public_subnet.id 
+    allocation_id = aws_eip.nat_eip.id 
+    tags = {
+        Name = "nat"
+    }
+}
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.my_vpc.id
+    route {
+        gateway_id = aws_internet_gateway.IGW.id 
+        cidr_block = "0.0.0.0/0"
+    }
+    tags = {
+        Name = "public_rt"
+    }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+    subnet_id = aws_subnet.public_subnet.id 
+    route_table_id = aws_route_table.public_rt.id 
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.my_vpc.id 
+    route {
+        nat_gateway_id = aws_nat_gateway.nat.id 
+        cidr_block = "0.0.0.0/0"
+    }
+    tags = {
+        Name = "private_rt"
+    }
+  
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+    subnet_id = aws_subnet.private_subnet.id 
+    route_table_id = aws_route_table.private_rt.id 
+}
+
+resource "aws_security_group" "sg" {
+    name = var.sg_name
+    description = var.sg_name
+    vpc_id = aws_vpc.my_vpc.id 
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "my_sg"
+    }
+}
---
+++ day-terraform-var-file/module/vpc.tf/var.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr" {}
+variable "public_subnet_cidr {}
+variable "private_subnet_cidr {}
+variable "public_az" {}
+variable "private_az" {}
+variable "ssh_port" {}
+variable "http_port" {}
+variable "sg_name" {}

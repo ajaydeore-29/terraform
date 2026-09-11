5a451eaa0f4d8c8e03139584faf9a00eef23c57e
ajaydeore-29 <ajaydeore8790@gmail.com>
2026-09-11 22:57:07 +0530

    fix: correct formatting and variable references in main.tf for S3 bucket resource

--- day-7-workspace/main.tf
+++ day-7-workspace/main.tf
@@ -1,11 +1,11 @@
 resource "random_id" "random_id" {
-    byte_length = 8
+  byte_length = 8
 }
 
 resource "aws_s3_bucket" "s3" {
-    bucket = "demo-bucket-${terraform.workspace}-${random-id.random-id.hex}"
-    tags = {
-        Name = "demo_bucket-${terraform.workspace}"
+  bucket = "demo-bucket-${terraform.workspace}-${random_id.random_id.hex}"
 
-    }
+  tags = {
+    Name = "demo_bucket-${terraform.workspace}"
+  }
 }
